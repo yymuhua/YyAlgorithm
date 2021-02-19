c6fec8ff5cdc9b960aa61df1ba15df2b9100bdef
YYmuhua <yymuhua@163.com>
2021-02-19 21:46:47 +0800

    update

--- algorithm/test/com/yyh/Test.java
+++ algorithm/test/com/yyh/Test.java
@@ -11,5 +11,6 @@ public class Test {
     public static Solution sol = new Solution();
     public static void main(String[] args) {
         System.out.println(sol.convert("leetcode", 3));
+
     }
 }
