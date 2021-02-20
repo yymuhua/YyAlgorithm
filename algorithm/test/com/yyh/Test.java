ecf87b5ab1d887dd5b17d86f029a71aedd3e7e7e
YYmuhua <yymuhua@163.com>
2021-02-20 15:14:44 +0800

    A

--- algorithm/test/com/yyh/Test.java
+++ algorithm/test/com/yyh/Test.java
@@ -11,6 +11,6 @@ public class Test {
     public static Solution sol = new Solution();
     public static void main(String[] args) {
         System.out.println(sol.convert("leetcode", 3));
-
+        System.out.println("A");
     }
 }
